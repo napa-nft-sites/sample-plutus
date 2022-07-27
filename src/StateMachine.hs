56b241a4148b62e2c1d4dbebdc456f8b135e279a
tiennhatdn98 <nhat.truongdn98@gmail.com>
2022-07-27 16:50:43 +0700

    initial

---
+++ src/StateMachine.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE DeriveAnyClass        #-}
+{-# LANGUAGE DeriveGeneric         #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NoImplicitPrelude     #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeApplications      #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeOperators         #-}
+
+{-# OPTIONS_GHC -g -fplugin-opt PlutusTx.Plugin:coverage-all #-}
+
+module Week08.TokenSale
+    ( TokenSale (..)
+    , TSRedeemer (..)
+    , tsCovIdx
+    , TSStartSchema
+    , TSUseSchema
+    , startEndpoint
+    , useEndpoints
+    , useEndpoints'
+    ) where
+
+import           Control.Monad                hiding (fmap)
+import           Data.Aeson                   (FromJSON, ToJSON)
+import           Data.Monoid                  (Last (..))
+import           Data.Text                    (Text, pack)
+import           GHC.Generics                 (Generic)
+import           Plutus.Contract              as Contract
+import           Plutus.Contract.StateMachine
+import qualified PlutusTx
+import           PlutusTx.Code                (getCovIdx)
+import           PlutusTx.Coverage            (CoverageIndex)
+import           PlutusTx.Prelude             hiding (Semigroup(..), check, unless)
+import           Ledger                       hiding (singleton)
+import           Ledger.Ada                   as Ada
+import           Ledger.Constraints           as Constraints
+import qualified Ledger.Typed.Scripts         as Scripts
+import           Ledger.Value
+import           Prelude                      (Semigroup (..), Show (..), uncurry)
+import qualified Prelude
